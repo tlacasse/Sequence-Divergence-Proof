9d4741ea0e5d9cbc9b20e52fd89fb6db66a0ef8b
Thomas Lacasse <thomas@kukt.us>
2019-04-30 03:16:00 -0400

    command line script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-pip install sympy
 pip install numpy
+pip install sympy
+pip install tensorflow
 pip install keras
+read -n 1 -s -r -p "Press any key to continue..."
---
+++ src/prove_diverge.py
@@ -0,0 +1,33 @@
+from argparse import ArgumentParser
+
+from problems import SeqFraction
+from prover import Prover
+from search import DFSSearch, NeuralNetworkSearch
+
+def run_proof(prover, top, bot, nopdf=False, proofname='proof'):
+    problem = SeqFraction(top, bot)
+    proof = prover.proof_search(problem)
+    if (not nopdf):
+        proof.write_proof_file(proofname)
+    
+def build_prover(search_method, printsearch):
+    search_obj = None
+    if (search_method == 'dfs'):
+        search_obj = DFSSearch()
+    if (search_method == 'nn'):
+        search_obj = NeuralNetworkSearch() 
+    prover = Prover(search_obj)
+    prover.print = printsearch
+    return prover
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('top')
+    parser.add_argument('bot')
+    parser.add_argument('--search')
+    parser.add_argument('--nopdf', action='store_true')
+    parser.add_argument('--printsearch', action='store_true')
+    args = parser.parse_args()
+    
+    p = build_prover(args.search, args.printsearch)
+    run_proof(p, args.top, args.bot, nopdf=args.nopdf)
